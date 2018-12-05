3916df98bc7e4099f48437288bb39f49c7d72672
William Lake <>
2018-12-05 06:27:29 -0700

    Minor changes

--- MainApp.py
+++ MainApp.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
 
     # headers = {'Accept': 'application/json'}
     
-    response = requests.get('https://marketdata.websol.barchart.com/getNews.json?apikey=769916ee01d11c2065d9980464e8b030&sources=AP%2CIF&symbols=AMZN%2CGOOG%2CAAPL&category=stocks&subCategory=tech&series=MORNCALL&keyword=tablet&maxRecords=10&startDate=2018-12-03T10%3A47%3A39&displayType=preview&images=true&storyId=259220&rss=false&fields=publishDateHost:%20ondemand.websol.barchart.comPOSTPOST%20https://ondemand.websol.barchart.com/getNews.jsonHost:%20ondemand.websol.barchart.comContent-Type:%20application/x-www-form-urlencodedContent-Length:%20lengthapikey=YOUR_API_KEY&sources=AP%2CIF&symbols=AMZN%2CGOOG%2CAAPL&category=stocks&subCategory=tech&series=MORNCALL&keyword=tablet&maxRecords=10&startDate=2018-12-03T10%3A47%3A39&displayType=preview&images=true&storyId=259220&rss=false&fields=publishDateResponse{%22status%22:%20{%22code%22:%20200,%22message%22:%20%22Success.%22},%22results%22:%20[{%22newsID%22:%20null,%22timestamp%22:%20null,%22source%22:%20null,%22categories%22:%20[],%22subCategories%22:%20[],%22headline%22:%20null,%22isExternal%22:%20null,%22publishDate%22:%20null,%22imageURL%22:%20null,%22imageCaption%22:%20null,%22imageHeight%22:%20null,%22imageWidth%22:%20null,%22preview%22:%20null,%22headlineURL%22:%20null,%22pdfURL%22:%20null}]}') # , headers=headers
+    response = requests.get('https://marketdata.websol.barchart.com/getNews.json?apikey=769916ee01d11c2065d9980464e8b030&symbols=NVDA') # , headers=headers
 
     code = response.status_code
 
